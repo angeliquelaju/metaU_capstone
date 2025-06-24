b4703752b11fdad63f2d454e02c8d7d66f1f4cec
Valerie Laju <avalerielaju@meta.com>
2025-06-24 16:41:36 -0700

    postgreSQL database & basic authentication implemented

---
+++ src/controllers/authController.ts
@@ -0,0 +1,71 @@
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import prisma from "../prisma";
+
+declare module "express-session" {
+  interface SessionData {
+    user: { username: string };
+  }
+}
+
+export const register = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  try {
+    const existingUser = await prisma.user.findUnique({ where: { username } });
+    if (existingUser) {
+      res.status(400).json({ message: "User exists" });
+      return;
+    }
+    const hashed = await bcrypt.hash(password, 10);
+    await prisma.user.create({
+      data: {
+        username,
+        password: hashed,
+      },
+    });
+    res.status(201).json({ message: "Signup successful!" });
+  } catch (error) {
+    console.error("Registration error:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({ where: { username } });
+    if (!user) {
+      res.status(401).json({ message: "Invalid credentials" });
+      return;
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      res.status(401).json({ message: "Invalid credentials" });
+      return;
+    }
+    req.session.user = { username };
+    res.json({ username });
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const me = (req: Request, res: Response) => {
+  if (req.session.user) {
+    res.json(req.session.user.username);
+    return;
+  }
+  res.status(401).json({ message: "Not logged in" });
+};
+
+export const logout = (req: Request, res: Response) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to log out" });
+    }
+    res.clearCookie("connect.sid");
+    res.json({ message: "Logout successful" });
+  });
+};

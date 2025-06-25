50c0d7901042565d18c287e3bd288977ba589a74
Angelique Valerie Laju <142268824+angeliquelaju@users.noreply.github.com>
2025-06-25 15:24:57 -0700

    Revert "Implemented PostgreSQL with Prisma + Login/Sign Up "

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -6,7 +6,7 @@ declare module "express-session" {
   interface SessionData {
     user: { username: string };
   }
-} 
+}
 
 export const register = async (req: Request, res: Response) => {
   const { username, password } = req.body;
@@ -53,11 +53,11 @@ export const login = async (req: Request, res: Response) => {
 };
 
 export const me = (req: Request, res: Response) => {
-  if (!req.session.user) {
-    res.status(401).json({ message: "Not logged in" });
+  if (req.session.user) {
+    res.json(req.session.user.username);
     return;
   }
-  res.json(req.session.user.username);
+  res.status(401).json({ message: "Not logged in" });
 };
 
 export const logout = (req: Request, res: Response) => {
@@ -69,11 +69,3 @@ export const logout = (req: Request, res: Response) => {
     res.json({ message: "Logout successful" });
   });
 };
-
-export const getSession = (req: Request, res: Response) => {
-  if (req.session.user) {
-    res.json({username: req.session.user});
-  } else {
-    res.status(401).json({message: "Not logged in"});
-  }
-}

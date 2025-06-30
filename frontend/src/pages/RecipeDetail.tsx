6c90d7cbbe48cf9b06428017843729808f13e85e
Valerie Laju <avalerielaju@meta.com>
2025-06-30 13:49:38 -0700

    added nutrition information, diet n dish type for each recipe display

--- frontend/src/pages/RecipeDetail.tsx
+++ frontend/src/pages/RecipeDetail.tsx
@@ -13,7 +13,7 @@ const RecipeDetail = () => {
       try {
         const apiKey = "c25d82b4400b4ec99e8dac172c6746d0";
         const res = await fetch(
-          `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`
+          `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}&includeNutrition=true`
         );
         if (!res.ok) throw new Error("failed to fetch recipes");
         const data = await res.json();
@@ -33,21 +33,56 @@ const RecipeDetail = () => {
 
   return (
     <div className="recipe-details-container">
-        <button className = "back" onClick={() => navigate(-1)}>Back</button>
-        <h1>{recipe.title}</h1>
-        <img src={recipe.image} alt={recipe.title}/>
-        <p><strong>Servings:</strong> {recipe.servings} people</p>
-        <p><strong>Ready in:</strong> {recipe.readyInMinutes} minutes</p>
+      <button className="back" onClick={() => navigate(-1)}>
+        Back
+      </button>
+      <h1>{recipe.title}</h1>
+      <img src={recipe.image} alt={recipe.title} />
+      <p>
+        <strong>Servings:</strong> {recipe.servings} people
+      </p>
+      <p>
+        <strong>Ready in:</strong> {recipe.readyInMinutes} minutes
+      </p>
 
-        <h2>Ingredients</h2>
-        <ul>
-            {recipe.extendedIngredients.map((ingredient: any) => (
-                <li key={ingredient.id}>{ingredient.original}</li>
-            ))}
-        </ul>
+      <h2>Ingredients</h2>
+      <ul>
+        {recipe.extendedIngredients.map((ingredient: any) => (
+          <li key={ingredient.id}>{ingredient.original}</li>
+        ))}
+      </ul>
 
-        <h2>Instructions</h2>
-        <p>{recipe.instructions || 'No instructions provided'}</p>
+      <h2>Instructions</h2>
+      <p>{recipe.instructions || "No instructions provided"}</p>
+
+      <h2>Other Information</h2>
+      <h4>Nutrition</h4>
+      <ul>
+        {recipe.nutrition?.nutrients?.map((nutrient: any) => (
+          <li key={nutrient.name}>
+            {nutrient.name}: {nutrient.amount}
+            {nutrient.unit}
+          </li>
+        ))}
+      </ul>
+
+      <h4>Diet</h4>
+      <ul>
+        {recipe.diet?.length > 0 ? (
+          recipe.diets.map((diet: string) => <li key={diet}>{diet}</li>)
+        ) : (
+          <p>no diet tags</p>
+        )}
+      </ul>
+
+      <h4>Dish Types</h4>
+      <ul>
+        {recipe.dishTypes?.length > 0 ? (
+          recipe.dishTypes.map((type: string) => <li key={type}>{type}</li>)
+        ) : (
+          <p>no types</p>
+        )}
+      </ul>
     </div>
   );
 };

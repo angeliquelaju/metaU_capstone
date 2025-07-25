e024ffcb6d0d163442d971da7e8c41e70d15efbc
Valerie Laju <avalerielaju@meta.com>
2025-07-25 00:53:06 -0700

    dist files

--- backend/dist/routes/recipes.js
+++ backend/dist/routes/recipes.js
@@ -17,240 +17,11 @@ const requireAuth_1 = __importDefault(require("../middleware/requireAuth"));
 const prisma_1 = __importDefault(require("../prisma"));
 const recipeMap_1 = require("../utils/recipeMap");
 const similarity_1 = require("../utils/similarity");
-require("express-session");
 const SPOON_KEY = process.env.SPOON_KEY;
-//saving recipes
 const router = (0, express_1.Router)();
-router.post("/recipes/save", requireAuth_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a;
-    const { id, title, image, ingredients } = req.body;
-    const username = (_a = req.session.user) === null || _a === void 0 ? void 0 : _a.username;
-    if (!username) {
-        res.status(401).json({ error: "not logged in" });
-        return;
-    }
-    try {
-        const user = yield prisma_1.default.user.findUnique({
-            where: { username },
-        });
-        if (!user) {
-            res.status(404).json({ error: "user not found" });
-            return;
-        }
-        const alreadySaved = yield prisma_1.default.recipe.findFirst({
-            where: {
-                id,
-                userSaves: {
-                    some: {
-                        id: user.id,
-                    },
-                },
-            },
-        });
-        if (alreadySaved) {
-            res.status(409).json({ error: "recipe already saved" });
-            return;
-        }
-        const recipe = yield prisma_1.default.recipe.upsert({
-            where: { id },
-            update: { ingredients: (ingredients === null || ingredients === void 0 ? void 0 : ingredients.length) ? ingredients : undefined },
-            create: {
-                id,
-                title,
-                image,
-                ingredients: (ingredients === null || ingredients === void 0 ? void 0 : ingredients.length) ? ingredients : [],
-            },
-        });
-        yield prisma_1.default.user.update({
-            where: { id: user.id },
-            data: {
-                saved: {
-                    connect: { id: recipe.id },
-                },
-            },
-        });
-        res.status(200).json({ message: "recipe saved" });
-    }
-    catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "internal error" });
-    }
-}));
-//getting the recipes that have been saved by a user
-router.get("/recipes/user", requireAuth_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a, _b;
-    const username = (_a = req.session.user) === null || _a === void 0 ? void 0 : _a.username;
-    if (!username) {
-        res.status(401).json({ error: "not logged in" });
-        return;
-    }
-    try {
-        const userWithRecipes = yield prisma_1.default.user.findUnique({
-            where: { username },
-            include: { saved: true },
-        });
-        res.json((_b = userWithRecipes === null || userWithRecipes === void 0 ? void 0 : userWithRecipes.saved) !== null && _b !== void 0 ? _b : []);
-    }
-    catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "internal error" });
-    }
-}));
-//delete recipes that have been saved
-router.delete("/recipes/remove/:id", requireAuth_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a;
-    const username = (_a = req.session.user) === null || _a === void 0 ? void 0 : _a.username;
-    const { id } = req.params;
-    if (!username) {
-        res.status(401).json({ error: "not logged in" });
-        return;
-    }
-    try {
-        const user = yield prisma_1.default.user.findUnique({
-            where: { username },
-        });
-        if (!user) {
-            res.status(404).json({ error: "user not found" });
-            return;
-        }
-        yield prisma_1.default.user.update({
-            where: { id: user.id },
-            data: {
-                saved: {
-                    disconnect: { id },
-                },
-            },
-        });
-        res.status(200).json({ message: "recipe removed" });
-    }
-    catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "internal error" });
-    }
-}));
-//liking recipes
-router.post("/recipes/like", requireAuth_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a;
-    const { id, title, image, ingredients } = req.body;
-    const username = (_a = req.session.user) === null || _a === void 0 ? void 0 : _a.username;
-    if (!username) {
-        res.status(401).json({ error: "not logged in" });
-        return;
-    }
-    try {
-        const user = yield prisma_1.default.user.findUnique({
-            where: { username },
-        });
-        if (!user) {
-            res.status(404).json({ error: "user not found" });
-            return;
-        }
-        const existingRecipe = yield prisma_1.default.recipe.findUnique({
-            where: { id },
-        });
-        let updatedRecipe;
-        if (existingRecipe) {
-            if (!existingRecipe.ingredients ||
-                existingRecipe.ingredients.length === 0) {
-                updatedRecipe = yield prisma_1.default.recipe.update({
-                    where: { id },
-                    data: { ingredients },
-                });
-            }
-            else {
-                updatedRecipe = existingRecipe;
-            }
-        }
-        else {
-            updatedRecipe = yield prisma_1.default.recipe.create({
-                data: { id, title, image, ingredients },
-            });
-        }
-        const alreadyLiked = yield prisma_1.default.recipe.findFirst({
-            where: {
-                id,
-                userLikes: {
-                    some: { id: user.id },
-                },
-            },
-        });
-        if (alreadyLiked) {
-            res.status(409).json({ error: "recipe liked already" });
-            return;
-        }
-        yield prisma_1.default.user.update({
-            where: { id: user.id },
-            data: {
-                liked: {
-                    connect: { id: updatedRecipe.id },
-                },
-            },
-        });
-        res.status(200).json({ message: "recipe liked" });
-    }
-    catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "server error" });
-    }
-}));
-//getting the recipes that have been liked
-router.get("/recipes/likedRecipes", requireAuth_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a;
-    const username = (_a = req.session.user) === null || _a === void 0 ? void 0 : _a.username;
-    if (!username) {
-        res.status(401).json({ error: "not logged in" });
-        return;
-    }
-    try {
-        const userwithLiked = yield prisma_1.default.user.findUnique({
-            where: { username },
-            include: { liked: true },
-        });
-        if (!userwithLiked) {
-            res.status(404).json({ error: "user not found" });
-            return;
-        }
-        res.status(200).json(userwithLiked.liked);
-        return;
-    }
-    catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "internal error" });
-    }
-}));
-//unlike recipes
-router.delete("/recipes/unlike/:id", requireAuth_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a;
-    const username = (_a = req.session.user) === null || _a === void 0 ? void 0 : _a.username;
-    const { id } = req.params;
-    if (!username) {
-        res.status(401).json({ error: "not logged in" });
-        return;
-    }
-    try {
-        const user = yield prisma_1.default.user.findUnique({
-            where: { username },
-        });
-        if (!user) {
-            res.status(404).json({ error: "user not found" });
-            return;
-        }
-        yield prisma_1.default.user.update({
-            where: { id: user.id },
-            data: {
-                liked: {
-                    disconnect: { id },
-                },
-            },
-        });
-        res.status(200).json({ message: "recipe unliked" });
-    }
-    catch (err) {
-        console.error(err);
-        res.status(500).json({ error: "internal error" });
-    }
-}));
-//getting personalized recipes
+//getting personalized recipes based on user's top 5 ingredients of their liked recipes
+//looks through liked recipes, gets all the ingredients from those recipes then groups them together 
+//gets the top 5 ingredients, then call spoonacular API to find recipes with those 5 ingredients
 router.get("/recipes/personalized", requireAuth_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     var _a;
     const username = (_a = req.session.user) === null || _a === void 0 ? void 0 : _a.username;
@@ -273,10 +44,11 @@ router.get("/recipes/personalized", requireAuth_1.default, (req, res) => __await
             res.status(400).json({ error: "no ingredients" });
             return;
         }
-        //group ingredient words (ex. chicken breast -> chicken)
+        //splits ingredient words (ex. chicken breast -> chicken, breast)
         const tokens = allIngredients.flatMap((ing) => 
         //.trim().split(/\s+/) takes out all whitespaces and splits it no matter what whitespace there is
         ing.trim().toLowerCase().split(/\s+/));
+        //acts as a counter for the ingredients
         const frequency = {};
         for (const token of tokens) {
             frequency[token] = (frequency[token] || 0) + 1;
@@ -286,7 +58,7 @@ router.get("/recipes/personalized", requireAuth_1.default, (req, res) => __await
             .slice(0, 5)
             .map(([key]) => key)
             .join(",");
-        const response = yield fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.SPOON_KEY}&includeIngredients=${topIngredients}&number=10&addRecipeInformation=true`);
+        const response = yield fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${SPOON_KEY}&includeIngredients=${topIngredients}&number=10&addRecipeInformation=true`);
         if (!response.ok)
             throw new Error("failed to get personalized recipes");
         const data = yield response.json();
@@ -297,44 +69,8 @@ router.get("/recipes/personalized", requireAuth_1.default, (req, res) => __await
         res.status(500).json({ error: "server error" });
     }
 }));
-//get other user's saved recipes using username
-router.get("/recipes/user/:username", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { username } = req.params;
-    try {
-        const user = yield prisma_1.default.user.findUnique({
-            where: { username },
-            include: { saved: true },
-        });
-        if (!user) {
-            res.status(404).json({ error: "user not found" });
-            return;
-        }
-        res.json(user.saved);
-    }
-    catch (err) {
-        console.error("error in public user recipe fetch: ", err);
-        res.status(500).json({ error: "internal error" });
-    }
-}));
-//gets users patterns (see how they interact with all the recipes) not just the ones that overlap
-//how much recipes a user has interacted with does not effect the score as it uses magnitude
-function cosineSimilarity(a, b) {
-    const allRecipeIds = new Set([...a.keys(), ...b.keys()]);
-    let dot = 0;
-    let magnitudeA = 0;
-    let magnitudeB = 0;
-    for (const id of allRecipeIds) {
-        const valA = a.get(id) || 0;
-        const valB = b.get(id) || 0;
-        dot += valA * valB; //calculate how much users interact with the same recipe
-        //overall interaction for that user
-        magnitudeA += valA ** 2;
-        magnitudeB += valB ** 2;
-    }
-    if (magnitudeA === 0 || magnitudeB === 0)
-        return 0;
-    return dot / (Math.sqrt(magnitudeA) * Math.sqrt(magnitudeB)); //final similarity score (closer to 1 more similar they are)
-}
+//returns recipes saved by the current user's suggested users that they have not interacted with
+//displays a match score for that recipe comparing it to the current user and how similar they are to the user who saved/liked it
 router.get("/recipes/recommended", requireAuth_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     var _a;
     const username = (_a = req.session.user) === null || _a === void 0 ? void 0 : _a.username;
@@ -376,7 +112,7 @@ router.get("/recipes/recommended", requireAuth_1.default, (req, res) => __awaite
                     otherRecipe.set(liked.id, liked);
                 }
             }
-            const sim = cosineSimilarity(userSaved, otherSaved); //gets similarity score for current user and other user
+            const sim = (0, similarity_1.cosineSimilarity)(userSaved, otherSaved); //gets similarity score for current user and other user
             if (sim === 0)
                 continue; //skip users who have 0 similarity score
             for (const [recipeId, score] of otherSaved.entries()) {

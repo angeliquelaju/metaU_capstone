07dcef4ae23d0ad85b96d72f89d4bcaba8f83c29
Angelique Valerie Laju <142268824+angeliquelaju@users.noreply.github.com>
2025-07-25 16:37:11 -0700

    Update Home.tsx
    added more instructions to the home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>welcome to find me recipes</h1>;
+  return (
+    <>
+      <h1>welcome to find me recipes</h1>
+      <h3>please log in or register to see the features of this site</h3>
+    </>
+  )
 }
